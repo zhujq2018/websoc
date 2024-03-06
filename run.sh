00aaed5e4475374419532162878aa7adfd1be659
zhujq2018 <78999765+zhujq2018@users.noreply.github.com>
2024-03-06 11:55:41 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,4 +12,4 @@ echo 'PS1='"'"'${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\
 
 wget -O /usr/local/bin/websocat https://github.com/vi/websocat/releases/download/v1.12.0/websocat.x86_64-unknown-linux-musl
 
-websocat --binary ws-l:127.0.0.1:8080 tcp:127.0.0.1:22
+websocat --binary ws-l:10.16.18.69:8080 tcp:127.0.0.1:22
